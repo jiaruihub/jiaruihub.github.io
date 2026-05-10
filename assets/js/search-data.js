88fc600bc196c754f1ea6a0fb6a00b3e00086f2d
jiaruihub <hubery.hu@colorado.edu>
2026-05-10 20:39:58 +0000

    Deploying to gh-pages from @ jiaruihub/jiaruihub.github.io@ce00a2b63459bd3931436733e6b097615a970260 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -380,7 +380,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "teachings-data-science-fundamentals",
+          section: "News",},{id: "projects-poetry",
+          title: 'Poetry',
+          description: "A collection of my poems on AllPoetry.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/poetry/";
+            },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
           section: "Teachings",handler: () => {
@@ -431,13 +436,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://www.linkedin.com/in/huberyhu", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
